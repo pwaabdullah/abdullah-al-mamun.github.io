1d0f78984080e5239bc6f277a0455ed5a1a90987
pwaabdullah <aamcse@gmail.com>
2020-06-05 14:47:03 -0400

    new template added

--- public/scripts/page.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var vizHeader = document.getElementById('viz_header');
-
-vizHeader.addEventListener('mouseover', function(e) {
-	vizHeader.classList.add("animated");
-	vizHeader.classList.add("bounce");
-});
